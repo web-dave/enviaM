c058f84c5abd1dda761962c96ae6775b6d6f4c3b
Dave <dave@webdave.de>
2019-03-06 18:35:32 +0100

    ready for unittesting in stackblitz

---
+++ src/tests.sb.ts
@@ -0,0 +1,16 @@
+import './app/app.component.spec.ts';
+import './app/side-nav/side-nav.component.spec.ts';
+
+// About
+import './app/about/about/about.component.spec.ts';
+
+// Books
+import './app/books/books.component.spec.ts';
+import './app/books/book-detail/book-detail.component.spec.ts';
+import './app/books/book-edit/book-edit.component.spec.ts';
+import './app/books/book-list/book-list.component.spec.ts';
+import './app/books/book-new/book-new.component.spec.ts';
+import './app/books/book-preview/book-preview.component.spec.ts';
+
+// Service
+import './app/books/shared/book-data.service.spec.ts';

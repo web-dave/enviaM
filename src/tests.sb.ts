08a3843f54d4e5de1ad94c44c81ef17ea789061e
Dave <dave@webdave.de>
2019-03-06 18:41:00 +0100

    fix spec path

--- src/tests.sb.ts
+++ src/tests.sb.ts
@@ -1,16 +1,16 @@
 import './app/app.component.spec.ts';
-import './app/side-nav/side-nav.component.spec.ts';
+import './app/my-nav/my-nav.component.spec.ts';
 
 // About
-import './app/about/about/about.component.spec.ts';
+import './app/about/about.component.spec.ts';
 
 // Books
 import './app/books/books.component.spec.ts';
-import './app/books/book-detail/book-detail.component.spec.ts';
+import './app/books/book-details/book-details.component.spec.ts';
 import './app/books/book-edit/book-edit.component.spec.ts';
 import './app/books/book-list/book-list.component.spec.ts';
 import './app/books/book-new/book-new.component.spec.ts';
 import './app/books/book-preview/book-preview.component.spec.ts';
 
 // Service
-import './app/books/shared/book-data.service.spec.ts';
+import './app/books/shared/book.service.spec';
